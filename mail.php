a83091380017cb2ca4fd83c9c2b1b93454710530
210591 <210591@hkis.edu.hk>
2016-02-18 19:41:41 +0800

    form

--- mail.php
+++ mail.php
@@ -1,10 +1,10 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-$dropdown = $_POST['dropdown'];
+$dropdown = $POST['dropdown'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "AKTnowHK@gmail.com;
+$recipient = "emailaddress@here.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
